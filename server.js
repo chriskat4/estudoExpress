4ae84916d91228121e4e030203f22adc39562413
Chris <christianpimpo304@hotmail.com>
2024-03-01 16:12:30 -0300

    v1

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ mongoose.connect(process.env.CONNECTIONSTRING, {useNewUrlParser:true , useUnifie
     })
     .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+
 
 
 const routes = require('./routes');
@@ -20,6 +24,19 @@ const { middlewareGlobal } = require('./src/middlewares/middleware');
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'uiodfhsafhusad',
+    store: new Mongostore({ mongooseConnection: mongoose.connection}),
+    resave: false,
+    saveUnitialized: false,
+    cokkie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,4 +1,6 @@
 exports.middlewareGlobal = (req, res, next) =>{
+    res.locals.umaVariavelLocal = 'valor da variavel local';
+
     console.log();
     
     if(req.body.cliente){
@@ -8,5 +10,9 @@ exports.middlewareGlobal = (req, res, next) =>{
 
     console.log();
     
+    next();
+};
+
+exports.outroMiddleware = (req, res, next)=>{
     next();
 };
